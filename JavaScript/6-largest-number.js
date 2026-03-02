ee5cb689e28f55129345d23efe7b2c12d3228f1c
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-02 21:37:14 +0200

    task 6

---
+++ JavaScript/6-largest-number.js
@@ -0,0 +1,14 @@
+function largest(num1, num2, num3) {
+  let largestNum;
+  if (num1 >= num2 && num1 >= num3) {
+    largestNum = num1;
+  } else if (num2 > num1 && num2 > num3) {
+    largestNum = num2;
+  } else {
+    largestNum = num3;
+  }
+
+  console.log(`The largest number is ${largestNum}.`);
+}
+
+largest(1, 2, 5);
