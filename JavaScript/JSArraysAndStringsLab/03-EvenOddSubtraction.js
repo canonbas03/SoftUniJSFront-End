bdb231c83060b8080b86098d231bb33144c7d9e5
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-13 09:50:14 +0200

    Create 03-EvenOddSubtraction.js

---
+++ JavaScript/JSArraysAndStringsLab/03-EvenOddSubtraction.js
@@ -0,0 +1,16 @@
+function solve(arr) {
+  let oddSum = 0;
+  let evenSum = 0;
+
+  for (let number of arr) {
+    if (number % 2 === 0) {
+      evenSum += number;
+    } else {
+      oddSum += number;
+    }
+  }
+
+  console.log(evenSum - oddSum);
+}
+
+solve([1, 2, 3, 4, 5, 6]);
