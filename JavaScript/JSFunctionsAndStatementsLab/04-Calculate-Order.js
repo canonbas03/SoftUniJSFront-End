e8161b5dd0b9d5ab96781d45c127e7c720d9d53a
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-18 20:55:17 +0200

    Create 04-Calculate-Order.js

---
+++ JavaScript/JSFunctionsAndStatementsLab/04-Calculate-Order.js
@@ -0,0 +1,23 @@
+// coffee - 1.50
+// · water - 1.00
+// · coke - 1.40
+// · snacks - 2.00
+function orderCalc(order, qty) {
+  let totalPrice = 0;
+  switch (order) {
+    case "coffe":
+      totalPrice = qty * 1.5;
+      break;
+    case "water":
+      totalPrice = qty * 1;
+      break;
+    case "coke":
+      totalPrice = qty * 1.4;
+      break;
+    case "snacks":
+      totalPrice = qty * 2;
+  }
+  console.log(totalPrice.toFixed(2));
+}
+
+orderCalc("coke", 5);
