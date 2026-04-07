3669ad143415c10b65f1ccdfcd59730fc6961817
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-07 08:55:44 +0300

    02

--- JavaScript/JSDOMIntroductionLab/02.Collect List Items/collect-list-items.js
+++ JavaScript/JSDOMIntroductionLab/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+  let liEls = document.querySelectorAll("li");
+  let textareaEl = document.querySelector("#result");
+
+  for (const li of liEls) {
+    let text = li.textContent;
+
+    textareaEl.textContent += text + "\n";
+  }
 }
