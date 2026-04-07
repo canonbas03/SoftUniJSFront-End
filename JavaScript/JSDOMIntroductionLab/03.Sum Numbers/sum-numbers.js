e8bfce9b9ca34bcef6d4257490f3b812e8245f2f
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-07 09:20:51 +0300

    03

--- JavaScript/JSDOMIntroductionLab/03.Sum Numbers/sum-numbers.js
+++ JavaScript/JSDOMIntroductionLab/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,11 @@
 function calc() {
-    // TODO
+  let num1InputEl = document.getElementById("num1");
+  let num2InputEl = document.getElementById("num2");
+  let sumInputEl = document.getElementById("sum");
+
+  let num1 = Number(num1InputEl.value);
+  let num2 = Number(num2InputEl.value);
+
+  let sum = num1 + num2;
+  sumInputEl.value = sum;
 }
