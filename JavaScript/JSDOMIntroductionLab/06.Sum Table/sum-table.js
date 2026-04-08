010b2ecfbecbfb35127de1f48484549ad491b54e
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-08 23:56:25 +0300

    Update sum-table.js

--- JavaScript/JSDOMIntroductionLab/06.Sum Table/sum-table.js
+++ JavaScript/JSDOMIntroductionLab/06.Sum Table/sum-table.js
@@ -1,3 +1,13 @@
 function sumTable() {
-    // TODO
+  const priceTdEls = Array.from(
+    document.querySelectorAll("tbody tr td:nth-child(2)"),
+  );
+  const sumTdEl = priceTdEls.pop();
+
+  let sum = 0;
+  for (const tdEl of priceTdEls) {
+    sum += Number(tdEl.textContent);
+  }
+
+  sumTdEl.textContent = sum;
 }
