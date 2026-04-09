c873e0a3aea0bf11d80e6b670a130c8f9a352026
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-09 07:00:05 +0300

    Update extract-parenthesis.js

--- JavaScript/JSDOMIntroductionLab/07.Extract Parenthesis/extract-parenthesis.js
+++ JavaScript/JSDOMIntroductionLab/07.Extract Parenthesis/extract-parenthesis.js
@@ -1,3 +1,9 @@
 function extract(content) {
-    // TODO
+  const el = document.getElementById(content);
+  const text = el.textContent;
+
+  const pattern = /\(.+?\)/g;
+
+  const matches = text.match(pattern);
+  return matches.map((match) => match.slice(1, -1)).join("; ");
 }
