352a7b810ce15bcd18af2d01f7a432d311384f7b
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-09 14:18:20 +0300

    Update search-in-list.js

--- JavaScript/JSDOMIntroductionExercise/04. Search in List/search-in-list.js
+++ JavaScript/JSDOMIntroductionExercise/04. Search in List/search-in-list.js
@@ -1,3 +1,35 @@
 function solve() {
-   // TODO
+  const townsListEls = document.querySelectorAll("#towns li");
+  const searchTextEl = document.getElementById("searchText");
+  const resultEl = document.getElementById("result");
+
+  clearOld();
+
+  let searchVal = searchTextEl.value;
+  let resultCount = 0;
+  townsListEls.forEach((townEl) => {
+    const isMatch = townEl.textContent
+      .toLowerCase()
+      .includes(searchVal.toLowerCase());
+
+    if (!isMatch) {
+      return;
+    }
+
+    townEl.style.textDecoration = "underline";
+    townEl.style.fontWeight = "bold";
+    townEl.style.opacity = 1;
+
+    resultCount++;
+  });
+
+  resultEl.textContent = `${resultCount} matches found`;
+
+  function clearOld() {
+    townsListEls.forEach((townEl) => {
+      townEl.style.textDecoration = "none";
+      townEl.style.fontWeight = "";
+      townEl.style.opacity = 0.5;
+    });
+  }
 }
