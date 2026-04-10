e4384df26fc208e75e79a71600a4ba9a126ca421
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-10 23:40:48 +0300

    Update mouse-gradient.js

--- JavaScript/JSDOMEvents/04. Mouse-Gradient/mouse-gradient.js
+++ JavaScript/JSDOMEvents/04. Mouse-Gradient/mouse-gradient.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    //TODO
+  const gradientDivEl = document.getElementById("gradient-box");
+  const resultDivEl = document.getElementById("result");
+
+  gradientDivEl.addEventListener("mousemove", handleMouseMove);
+
+  function handleMouseMove(e) {
+    const cursorDistanceFromLeft = e.offsetX;
+    const divElWidth = e.target.clientWidth;
+
+    const percent = Math.floor((cursorDistanceFromLeft / divElWidth) * 100);
+
+    resultDivEl.textContent = `${percent}%`;
+  }
 }
