becea2ea9bebef34ec055971d6bce71f6a4b3249
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-10 22:28:56 +0300

    Update list-of-items.js

--- JavaScript/JSDOMEvents/01. List-Of-Items/list-of-items.js
+++ JavaScript/JSDOMEvents/01. List-Of-Items/list-of-items.js
@@ -1,3 +1,11 @@
 function addItem() {
-    //TODO
+  const itemsUlEls = document.getElementById("items");
+  const newItemtextEl = document.getElementById("newItemText");
+
+  const text = newItemtextEl.value.trim();
+
+  const liEl = document.createElement("li");
+  liEl.textContent = text;
+
+  itemsUlEls.appendChild(liEl);
 }
