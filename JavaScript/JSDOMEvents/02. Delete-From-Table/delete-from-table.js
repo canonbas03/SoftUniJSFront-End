8140440975469e6641eef44c9e1a8e7f61d65694
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-10 22:55:05 +0300

    Update delete-from-table.js

--- JavaScript/JSDOMEvents/02. Delete-From-Table/delete-from-table.js
+++ JavaScript/JSDOMEvents/02. Delete-From-Table/delete-from-table.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
-    //TODO
+  const emailEls = document.querySelectorAll("tr td:nth-child(2)");
+  const emailInputEL = document.querySelector('input[name="email"]');
+  const resultEl = document.querySelector("#result");
+
+  const emailTextVal = emailInputEL.value.trim();
+
+  let isFound = false;
+  for (const email of emailEls) {
+    if (email.textContent === emailTextVal) {
+      email.parentElement.remove();
+      resultEl.textContent = "Deleted.";
+      isFound = true;
+    }
+  }
+
+  if (!isFound) {
+    resultEl.textContent = "Not found.";
+  }
 }
