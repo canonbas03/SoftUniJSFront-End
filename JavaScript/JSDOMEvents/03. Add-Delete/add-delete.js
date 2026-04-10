97fe9b688c904167f74de15f3bf70cf5d0ab7647
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-10 23:13:54 +0300

    Update add-delete.js

--- JavaScript/JSDOMEvents/03. Add-Delete/add-delete.js
+++ JavaScript/JSDOMEvents/03. Add-Delete/add-delete.js
@@ -1,3 +1,22 @@
 function addItem() {
-    // TODO
+  const itemsUlEls = document.getElementById("items");
+  const newItemtextEl = document.getElementById("newItemText");
+
+  const text = newItemtextEl.value.trim();
+
+  const liEl = document.createElement("li");
+  liEl.textContent = text;
+
+  const deleteAEl = document.createElement("a");
+  deleteAEl.href = "#";
+  deleteAEl.textContent = "[Delete]";
+
+  liEl.appendChild(deleteAEl);
+  itemsUlEls.appendChild(liEl);
+
+  deleteAEl.addEventListener("click", deleteListItem);
+
+  function deleteListItem(e) {
+    e.target.parentElement.remove();
+  }
 }
