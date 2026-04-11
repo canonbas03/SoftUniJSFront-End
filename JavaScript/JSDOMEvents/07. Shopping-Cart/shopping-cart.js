369dbc5be72e711d7d029d0d60d67df0531dae8d
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-11 13:28:38 +0300

    Update shopping-cart.js

--- JavaScript/JSDOMEvents/07. Shopping-Cart/shopping-cart.js
+++ JavaScript/JSDOMEvents/07. Shopping-Cart/shopping-cart.js
@@ -1,5 +1,48 @@
-document.addEventListener('DOMContentLoaded', solve);
+document.addEventListener("DOMContentLoaded", solve);
 
 function solve() {
-   //TODO
+  const allAddBtnEls = document.querySelectorAll(".add-product");
+  const resultTextareaEl = document.querySelector("textarea");
+  const checkoutBtnEl = document.querySelector(".checkout");
+
+  let uniqueProducts = new Set();
+  let totalPriceSum = 0;
+
+  for (const addBtn of allAddBtnEls) {
+    addBtn.addEventListener("click", handleAddProduct);
+  }
+
+  checkoutBtnEl.addEventListener("click", handleCheckout);
+
+  function handleAddProduct(e) {
+    const fullProductDivEl = e.target.parentElement.parentElement;
+
+    const productTitleDivEl = fullProductDivEl.querySelector(".product-title");
+    const productTitle = productTitleDivEl.textContent;
+
+    const productPriceDivEl = fullProductDivEl.querySelector(
+      ".product-line-price",
+    );
+    const productPrice = Number(productPriceDivEl.textContent);
+
+    resultTextareaEl.append(
+      `Added ${productTitle} for ${productPrice.toFixed(2)} to the cart.\n`,
+    );
+
+    uniqueProducts.add(productTitle);
+    totalPriceSum += productPrice;
+  }
+
+  function handleCheckout() {
+    const allProductsArr = Array.from(uniqueProducts);
+    resultTextareaEl.append(
+      `You bought ${allProductsArr.join(", ")} for ${totalPriceSum.toFixed(2)}.`,
+    );
+
+    checkoutBtnEl.disabled = true;
+
+    for (const addButton of allAddBtnEls) {
+      addButton.disabled = true;
+    }
+  }
 }
