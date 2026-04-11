b20494b6d2caec5a02089a7f2278878580482df2
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-11 11:52:29 +0300

    Update dynamic-validation.js

--- JavaScript/JSDOMEvents/06. Dynamic-Validation/dynamic-validation.js
+++ JavaScript/JSDOMEvents/06. Dynamic-Validation/dynamic-validation.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', solve);
+document.addEventListener("DOMContentLoaded", solve);
 
 function solve() {
-    //TODO
+  const emailInputEl = document.getElementById("email");
+
+  emailInputEl.addEventListener("change", handleEmailChange);
+
+  function handleEmailChange() {
+    const emailText = emailInputEl.value;
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+    if (!pattern.test(emailText)) {
+      emailInputEl.classList.add("error");
+    } else {
+      emailInputEl.classList.remove("error");
+    }
+  }
 }
