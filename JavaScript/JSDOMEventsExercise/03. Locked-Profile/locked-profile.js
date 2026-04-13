7c457dc8aeadf8ed53cb38f091088efad36d2ede
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-04-13 17:40:21 +0300

    Update locked-profile.js

--- JavaScript/JSDOMEventsExercise/03. Locked-Profile/locked-profile.js
+++ JavaScript/JSDOMEventsExercise/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,31 @@
-document.addEventListener('DOMContentLoaded', solve);
+document.addEventListener("DOMContentLoaded", solve);
 
 function solve() {
-    //TODO
+  const allShowBtns = document.querySelectorAll("button");
+
+  for (const showBtn of allShowBtns) {
+    showBtn.addEventListener("click", handleShowBtnClick);
+  }
+
+  function handleShowBtnClick(e) {
+    const currentShowBtn = e.currentTarget;
+    const currentProfile = currentShowBtn.parentElement;
+    const hiddenFields = currentProfile.querySelector(".hidden-fields");
+    const radioGroup = currentProfile.querySelector(".radio-group");
+    const lockRadioBtn = radioGroup.querySelector('input[type="radio"]');
+
+    let isLocked = lockRadioBtn.checked;
+
+    if (!isLocked) {
+      hiddenFields.classList.remove("active");
+      currentShowBtn.textContent = "Show less";
+      currentShowBtn.classList.remove("active");
+      currentShowBtn.disabled = true;
+    } else {
+      hiddenFields.classList.add("active");
+      currentShowBtn.textContent = "Show more";
+      currentShowBtn.classList.add("active");
+      currentShowBtn.disabled = false;
+    }
+  }
 }
